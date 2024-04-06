7165a6bcc9857c0684fdcca396e6358af2827dc1
amrito502 <amritobosu7@gmail.com>
2024-04-07 00:42:07 +0600

    Admin Theam Templating

---
+++ resources/views/admin/admin/list.blade.php
@@ -0,0 +1,14 @@
+@extends('admin.layouts.app')
+{{-- ========start-style====== --}}
+@section('style')
+@endsection
+{{-- ========end-style====== --}}
+@section('content')
+
+   
+
+@endsection
+{{-- ========scripts====== --}}
+@section('scripts')
+@endsection
+
